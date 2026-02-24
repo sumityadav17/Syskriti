7ceed536287c6dee72ae112d72dd1cde30fab3e8
sumityadav17 <43839444+sumityadav17@users.noreply.github.com>
2026-02-24 16:49:38 +0530

    chessboard

---
+++ Chessborad.py
@@ -0,0 +1,19 @@
+#  Taking input
+N = int(input("Enter value of N: "))
+
+#  Use 2 for loops for rows and columns N*N matrix
+for row in range(N):
+    for col in range(N):
+        
+        #  Apply conditions
+        if row == col:
+            print("X", end=" ")
+        
+        elif (row + col) % 2 == 0:
+            print("1", end=" ")
+        
+        else:
+            print("0", end=" ")
+
+ #  Print new line after each row 
+    print()
